a222ab8d054e345f3475986c52de8d3d26b2c8a8
zhengjinwei123 <2538698032@qq.com>
2017-06-10 13:37:44 +0800

    add koa examples

--- app.js
+++ app.js
@@ -22,15 +22,34 @@ app.use(function *(next){
 });
 
 app.use(function *(){
-   console.log("method:",this.method);
-   console.log("url:",this.url);
-   console.log("header:",this.header);
-   console.log("response:",this.response);
-   console.log("app:",this.app);
-   console.log("originalUrl:",this.originalUrl);
+   //console.log("method:",this.method);
+   //console.log("url:",this.url);
+   //console.log("header:",this.header);
+   //console.log("response:",this.response);
+   //console.log("app:",this.app);
+   //console.log("originalUrl:",this.originalUrl);
 
-   this.body  = 'Hello Worldnhehe';
+   //console.log("request:",this.request);
+
+   //console.log("response:",this.response);
+
+   //console.log(this.cookies);
+   //this.body  = 'Hello Worldnhehe';
+   //this.throw('name required', 400)
+   this.body = this.request.path;
+   this.body = this.request.querystring;
+   this.body = this.request.hostname;
+   this.body = this.request.type;
+   this.body = this.request.query;
+
+   this.response.body = "123";
+   //this.response.redirect("http://www.baidu.com");
+   //this.response.attachment(["./app.js"]);
 });
 
+
+app.on('error',function(err,context){
+   console.error(err,context);
+});
 app.listen(3000);
 app.listen(3001);
